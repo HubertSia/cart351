d3efc62719508f10e7a7469b65495c2772c064ae
HubertSia <hubemail2021@gmail.com>
2025-10-20 17:16:40 -0400

    finish t2 and added screengrabs

--- hubertsia-1__exercise_two/exercise-two.py
+++ hubertsia-1__exercise_two/exercise-two.py
@@ -11,29 +11,46 @@ def index():
 #*************************************************
 
 #Task: Variables and JinJa Templates
-# @app.route("/t1")
-# def t1():
-#       the_topic = "donuts"
-#       number_of_donuts = 28
-#       donut_data= {
-#       "flavours":["Regular", "Chocolate", "Blueberry", "Devil's Food"],
-#       "toppings": ["None","Glazed","Sugar","Powdered Sugar",
-#                    "Chocolate with Sprinkles","Chocolate","Maple"]
-#                    }
-      
-#       icecream_flavors = ["Vanilla","Raspberry","Cherry", "Lemon"]
-#       return render_template("t1.html")
+@app.route("/t1")
+def t1():
+    the_topic = "donuts"
+    number_of_donuts = 28
+    donut_data = {
+        "flavours": ["Regular", "Chocolate", "Blueberry", "Devil's Food"],
+        "toppings": ["None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple"]
+    }
 
+    icecream_flavors = ["Vanilla", "Raspberry", "Cherry", "Lemon"]
+
+    return render_template(
+        "t1.html",
+        the_topic=the_topic,
+        number_of_donuts=number_of_donuts,
+        donut_data=donut_data,
+        icecream_flavors=icecream_flavors
+    )
 #*************************************************
 
 #Task: HTML Form get & Data 
-# @app.route("/t2")
-# def t2():
-#     return render_template("t2.html")
+@app.route("/t2")
+def t2():
+    return render_template("t2.html")
 
-# @app.route("/thank_you_t2")
-# def thank_you_t2():
-#     return render_template("thankyou_t2.html")
+@app.route("/thank_you_t2")
+def thank_you_t2():
+    # get form inputs from URL parameters
+    name = request.args.get("name", "")
+    subject = request.args.get("subject", "")
+    message = request.args.get("message", "")
+
+    # combine them into one string
+    combined = f"{name} {subject} {message}"
+
+    # replace vowels with '*'
+    modified = "".join("*" if ch.lower() in "aeiou" else ch for ch in combined)
+
+    # pass result to template
+    return render_template("thankyou_t2.html", final_text=modified)
 
 #*************************************************
 
