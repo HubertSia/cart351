59074989778eb1af11fb9c4f0a721c32aafc2331
HubertSia <hubemail2021@gmail.com>
2025-11-08 02:37:45 -0500

    little improvements and adjustments

--- hubertsia_project2/static/js/script.js
+++ hubertsia_project2/static/js/script.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+function updateBackgroundFromData() {
+  fetch("/get_data")
+    .then((res) => res.json())
+    .then((data) => {
+      const forest = data.forest || 0;
+      const village = data.village || 0;
+      document.body.classList.remove("forest-mode", "village-mode");
+      if (forest > village) {
+        document.body.classList.add("forest-mode");
+      } else if (village > forest) {
+        document.body.classList.add("village-mode");
+      }
+    })
+    .catch((err) => console.error("Error fetching data:", err));
+}
+
+updateBackgroundFromData();
+
+setInterval(updateBackgroundFromData, 10000);
